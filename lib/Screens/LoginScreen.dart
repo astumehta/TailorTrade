2837465161aec25bc6e67bffc6219a6d53366427
astumehta <143540555+astumehta@users.noreply.github.com>
2024-03-16 20:00:08 +0530

    second commit, added password field and UI

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -10,11 +9,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
-  
-  final TextEditingController email=TextEditingController();
-  final TextEditingController password=TextEditingController();
-  
+  bool passsecure = false;
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -25,12 +22,14 @@ class _LoginScreenState extends State<LoginScreen> {
         child: SingleChildScrollView(
           child: Center(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start, // Aligns text to the left
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Aligns text to the left
               children: [
                 const Image(
-                  image: AssetImage("images/—Pngtree—flat design of tailor fashion_6101332.png"),
-                  width: 900,
-                  height: 400,
+                  image: AssetImage(
+                      "images/—Pngtree—flat design of tailor fashion_6101332.png"),
+                  width: 750,
+                  height: 350,
                 ),
                 Text(
                   "Welcome back!",
@@ -38,7 +37,8 @@ class _LoginScreenState extends State<LoginScreen> {
                   style: GoogleFonts.poppins(
                     fontSize: 30.0,
                     fontWeight: FontWeight.bold,
-                    color: const Color(0xff596E79), // Color property needs correction
+                    color: const Color(
+                        0xff596E79), // Color property needs correction
                   ),
                 ),
                 Text(
@@ -46,54 +46,81 @@ class _LoginScreenState extends State<LoginScreen> {
                   textAlign: TextAlign.left,
                   style: GoogleFonts.poppins(
                     fontSize: 17.0,
-                    color: const Color(0xff596E79), // Color property needs correction
+                    color: const Color(
+                        0xff596E79), // Color property needs correction
                   ),
                 ),
                 const SizedBox(height: 30),
-
-TextField(
-  controller: email, // Replace with your TextEditingController
+                TextField(
+                  controller: email, // Replace with your TextEditingController
+                  style: const TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                  ),
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 12.0, horizontal: 10.0),
+                    prefixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                          child: const Icon(Icons.person),
+                        ),
+                        const SizedBox(width: 1.0),
+                        // Add a Container with 1-width black vertical line
+                        Container(
+                          width: 1.0,
+                          color: Colors.black,
+                          margin: const EdgeInsets.symmetric(vertical: 10.0),
+                        ),
+                      ],
+                    ),
+        
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: const Color(0xffDFD3C3),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+  obscureText: passsecure,
+  controller: password,
   style: const TextStyle(
     color: Color.fromRGBO(0, 0, 0, 1),
   ),
   decoration: InputDecoration(
+    hintText: "Password",
+    
     contentPadding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 10.0),
-    prefixIcon: Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(10,0,0,0),
-          child: const Icon(Icons.person),
-        ),
-        const SizedBox(width: 1.0),
-        // Add a Container with 1-width black vertical line
-        Container(
-          width: 1.0,
-          color: Colors.black,
-          margin: const EdgeInsets.symmetric(vertical: 10.0),
-        ),
-      ],
-    ),
-    labelText: "Email",
-    labelStyle: const TextStyle(
-      color: Color.fromRGBO(128, 128, 128, 1),
-    ),
-    floatingLabelBehavior: FloatingLabelBehavior.always,
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(10.0), // Rounded corners
-      borderSide: BorderSide.none, // No border line
+    prefixIcon: const Icon(Icons.lock),
+    prefixIconColor: const Color.fromRGBO(0, 0, 0, 1),
+    suffixIcon: GestureDetector(
+      onTap: () {
+        setState(() {
+          passsecure = !passsecure;
+        });
+      },
+      child: Icon(
+        passsecure ? Icons.visibility_off : Icons.visibility,
+        color: Colors.black,
+      ),
     ),
     filled: true,
     fillColor: const Color(0xffDFD3C3),
+    floatingLabelBehavior: FloatingLabelBehavior.always,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0), 
+      borderSide: BorderSide.none, // Removing the border side
+    ),
+    
   ),
 ),
 
 
-
-
-
-
-                      const SizedBox(height: 20),
               ],
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -14,7 +13,10 @@ void main() async {
   AwesomeNotifications().initialize(
     null,
     [
-      NotificationChannel(channelKey: 'basic_channel', channelName: 'basic_notification', channelDescription: 'notification channel'),
+      NotificationChannel(
+          channelKey: 'basic_channel',
+          channelName: 'basic_notification',
+          channelDescription: 'notification channel'),
     ],
     debug: true,
   );
@@ -27,9 +29,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData.light(),
-        home: const SplashScreen());
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: const SplashScreen(),
+    );
   }
 }
 
@@ -42,26 +45,32 @@ class SplashScreen extends StatefulWidget {
 
 class SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() 
-  {
-AwesomeNotifications().isNotificationAllowed().then((isAllowed){
-  if(!isAllowed){
-    AwesomeNotifications().requestPermissionToSendNotifications();
-  }
-});
+  void initState() {
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: AnimatedSplashScreen(
-          splash: const Image(
-            image: AssetImage("images/hotel.gif"),
+      body: Container(
+        child: AnimatedSplashScreen(
+          splash: Center( 
+            child: Transform.scale(
+              scale: 3.0, 
+              child: Image.asset(
+                "images/de8axxt-7b8bb469-b579-4f9b-b946-d985f74f1d8f.gif",
+                fit: BoxFit.contain,
+              ),
+            ),
           ),
-          nextScreen: const LoginScreen()),
+          nextScreen: const LoginScreen(),
+        ),
+      ),
     );
   }
 }
-
